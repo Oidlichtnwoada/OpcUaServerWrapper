67ee0681113a7060968b588e9c78f5b1af59fa40
Hannes Brantner <e1614466@student.tuwien.ac.at>
2020-05-07 14:22:39 +0200

    start script was false, git pull was executed in /

--- update_repository_start_server.sh
+++ update_repository_start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/bin/git pull
 cd /home/pi/OpcUaServerWrapper
+/usr/bin/git pull
 /usr/bin/python3.7 /home/pi/OpcUaServerWrapper/startserver.py 192.168.162.84 4840 192.168.162.82 10003
