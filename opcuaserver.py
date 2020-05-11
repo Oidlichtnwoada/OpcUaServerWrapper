bffab6134faf5c71f497acd8e0a17629e473c545
STAstefan <steff.ad@gmx.at>
2020-05-11 19:35:29 +0200

    added python test for getErrorLog

--- opcuaserver.py
+++ opcuaserver.py
@@ -188,14 +188,18 @@ class OpcUaServerForRobotController:
     @uamethod
     def getErrorLog(self, parent, numLogs):
         try:
+            test = self.opc_ua_server.get_node('ns=4;i=1155')
+             objects = self.opc_ua_server.load_type_definitions(test)
+             test.ErrorDate = "Test1"
+            test.ErrorTime = "Test3"
+
             for i in range(numLogs):
                 if i == 0:
-                    test = self.opc_ua_server.get_node('ns=4;i=1155')
-                    test.ErrorDate = "Test1"
-                    test.ErrorTime = "Test3"
+                    
+                    
 
                     return test
-                    
+
                     print(self.rc_client.get_error_log("TOP"))
                 else:
                     print(self.rc_client.get_error_log("+1"))
