0867e45f4f840a6ce3c13bd9d44fdff9eb331058
Hannes Brantner <e1614466@student.tuwien.ac.at>
2020-05-13 17:27:04 +0200

    changed path

--- scripts/update_repository_and_start_server.sh
+++ scripts/update_repository_and_start_server.sh
@@ -2,4 +2,4 @@
 
 cd /home/pi/OpcUaServerWrapper/src
 /usr/bin/git pull
-/usr/bin/python3.7 /home/pi/OpcUaServerWrapper/startserver.py 192.168.162.84 4840 192.168.162.82 10003
+/usr/bin/python3.7 /home/pi/OpcUaServerWrapper/src/startserver.py 192.168.162.84 4840 192.168.162.82 10003
