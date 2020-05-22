bee19f26a9f32ba49db103318a5e8820a06ac731
Hannes Brantner <e01614466@student.tuwien.ac.at>
2020-05-22 07:26:19 +0200

    changed method read_input and write_output to use the boolean data type

--- src/robotcontrollerclient.py
+++ src/robotcontrollerclient.py
@@ -118,12 +118,12 @@ class RobotControllerClient:
                 sleep(1)
 
     def read_input(self, index, robot_number=1, slot_number=1):
-        return int(self.process_request(f'{robot_number};{slot_number};IN{index}')[0], 16) % 2
+        return bool(int(self.process_request(f'{robot_number};{slot_number};IN{index}')[0], 16) % 2)
 
     def write_output(self, index, value, robot_number=1, slot_number=1):
         output_value = int(self.process_request(f'{robot_number};{slot_number};OUT{index}')[0], 16)
         output_value &= ~1
-        output_value |= int(value > 0)
+        output_value |= int(value)
         self.process_request(f'{robot_number};{slot_number};OUT={index};{output_value:04x}')
 
     def open_gripper(self, hand_number=1, robot_number=1, slot_number=1):
